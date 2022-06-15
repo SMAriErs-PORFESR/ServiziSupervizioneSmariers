48090904de4c651ab42668c30d6d248139210587
Giorgio <imatesiu@gmail.com>
2022-06-16 00:52:51 +0200

    Update ReadSQLiteListIO.py

--- script/data/ReadSQLiteListIO.py
+++ script/data/ReadSQLiteListIO.py
@@ -8,6 +8,17 @@ import sqlite3
 if len(sys.argv)<2:
 	print('mancadb')
 	exit(0)
+	
+def scrivi(data,name,des,p):
+		with open(data+name.zfill(2)+"."+des+'.txt', 'w') as f:
+			for row in p:
+				if type ==1:#Codice=descizione,tipo,scala
+					line  = str(row[3])+"="+str(row[4])+","+str(row[5])+","+str(row[9])+""
+				else:
+					line  = str(row[3])+"="+str(row[4])+""
+				f.write(str(line)+'\r\n')
+				#f.write('\r\n')
+				
 argom  = sys.argv[1]
 con = sqlite3.connect(argom)
 
@@ -22,9 +33,23 @@ for row in cur.execute('SELECT * FROM LIST_IO ORDER BY APPMOD'):
 			listAPPMOD.append(currentappmod)
 		
 print(listAPPMOD)'''
-
+rename = {}
+for row in cur.execute('SELECT * FROM CONF_MODULE ORDER BY TYPEAPP'):
+		currenttypeapp = hex(abs(row[20]))
+		currenttdper = hex(abs(row[6]))
+		print(currenttypeapp,currenttdper)
+		if not  currenttypeapp in rename:
+			list = []
+			list.append(currenttdper)
+			rename[currenttypeapp]= list
+		else:
+			z = rename[currenttypeapp]
+			z.append(currenttdper)
+			
+#print(rename)		
+#exit(0)	
 for row in cur.execute('SELECT * FROM LIST_IO ORDER BY APPMOD'):
-		currentappmod = hex(row[2])
+		currentappmod = hex(abs(row[2]))
 		currenttype = row[1]
 		if not currentappmod in map:
 			dizio = {}
@@ -48,10 +73,18 @@ for element in map:
 		t = map[element]
 		p = t[type]
 		des = "analog"
+		#if type ==1: print(type,p[1],element[2:])
 		if type ==2: des = "output"	
 		if type ==0: des = "input"
 		if type ==3: des = "diagnostica"
 		name = element[2:]
+		scrivi(data,name,des,p)
+		for app in rename:
+			for f in rename[app]:
+				#print(f[2:])
+				if app[2:] == name:
+					scrivi(data,f[2:],des,p)
+		'''
 		with open(data+name.zfill(2)+"."+des+'.txt', 'w') as f:
 			for row in p:
 				if type ==1:#Codice=descizione,tipo,scala
@@ -60,5 +93,5 @@ for element in map:
 					line  = str(row[3])+"="+str(row[4])+""
 				f.write(str(line)+'\r\n')
 				#f.write('\r\n')
-			
+			'''
 
